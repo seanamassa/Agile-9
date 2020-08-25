93eb0c7666aec8b4b919dc4961362a8052ccbfd8
alejqe <69996582+alejqe@users.noreply.github.com>
2020-08-25 16:30:48 -0700

    update diver.cpp
    update on diver.cpp making it more suitable for the eye

--- driver.cpp
+++ driver.cpp
@@ -2,8 +2,17 @@
 #include <windows.h>
 using namespace std;
 
+void featureA();
+void featureB();
+void featureC();
+void featureD();
+
 int main()
 {
+  cout << "Welcome to our project." << endl;
+  
+  // might want to add something here to fill 
+  
   featureA();
   featureB();
   featureC();
